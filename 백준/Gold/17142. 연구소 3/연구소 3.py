a4d12b52a5e4898df49863d018294893debf2e91
Eom da yeon <69031678+eomdayeon@users.noreply.github.com>
2022-10-08 19:44:06 +0900

    [Gold IV] Title: 연구소 3, Time: 960 ms, Memory: 32556 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -3,6 +3,10 @@
 # 즉, 2의 개수가 K개라고 가정하면, K개 중에 M개를 뽑는 조합의 경우를 탐색한다.(dfs)
 # K개 중에 M개 뽑는 모든 경우의 수에 대해, 바이러스가 빈칸에 바이러스를 확산시킨다.(bfs) -> 언제까지? 모든 0이었던 칸이 활성화될 때 까지.
 # 모든 0이 탐색되었을 때 최종 결과를 return 해준다.
+
+# 실수한 부분!!!!! 시간을 기록하며 더해나가는 temp배열을 board배열로 복사해서 썼는데, 그러면 벽(1)과 바이러스(2)까지 모두 저장되어있어서 temp에 시간을 갱신해주면서 값이 꼬일 가능성이 있다.
+# 따라서, 앞으로는 bfs를 통해 기록을 할 때 새로운 배열로 기록을 해주는 것이 좋을 것이다.
+
 from collections import deque
 
 N, M = map(int, input().split())
@@ -31,8 +35,9 @@ def dfs(idx, cnt):
     if cnt == M:
         # 만약, M개 다 뽑았다면 그 M개 바이러스에 대해 bfs를 진행한다.
         res = bfs(sel_virus_idx)
-        if res < result:
-            result = res
+        result = min(result,res)
+        # if res < result:
+        #     result = res
         return
 
     for i in range(idx, len(virus)):
@@ -46,7 +51,7 @@ def bfs(sel_virus_idx):
     visited = [[False] * N for _ in range(N)]
     dx = [-1, 0, +1, 0]
     dy = [0, -1, 0, +1]
-    t = [[0]*N for _ in range(N)]
+    t = [[0]*N for _ in range(N)]   #처음에 board 배열을 temp배열에 모두 복제하고 썼는데, 그럴 필요가 없었다. 괜히 틀리기만 함. -> 시간을 저장하는 t 배열만 쓰면 되고, board배열에서 빈칸인지 비활성화된 바이러스인지 체크한다.
     time = 0
     blank = 0
 
@@ -63,26 +68,21 @@ def bfs(sel_virus_idx):
 
         cx, cy = queue.popleft()
 
-
         for k in range(4):
             nx, ny = cx + dx[k], cy + dy[k]
             if nx < 0 or ny < 0 or nx >= N or ny >= N:
                 continue
 
-            if board[nx][ny] == 0 and visited[nx][ny] == False:  # 0이고, 방문안한 경우에 큐에 삽입(즉, 바이러스를 퍼뜨림) -> 1초의 시간 소요
+            if board[nx][ny] != -1 and visited[nx][ny] == False:  # 0이고, 방문안한 경우에 큐에 삽입(즉, 바이러스를 퍼뜨림) -> 1초의 시간 소요
                 visited[nx][ny] = True
                 t[nx][ny] = t[cx][cy] + 1
                 queue.append((nx, ny))
-                blank += 1
                 time = t[nx][ny]
+                if board[nx][ny] == 0:
+                    blank += 1
 
-            elif board[nx][ny] == 2 and visited[nx][ny] == False:  # 비활성화 바이러스이고, 방문안한 경우에 활성화
-                visited[nx][ny] = True
-                t[nx][ny] = t[cx][cy] + 1
-                queue.append((nx, ny))
-
-
-    if blank < count:
+    
+    if blank < count:   #빈칸이 다 채워주지 못한 경우, 시간을 max로 주어야 한다. -> 이 코드를 안적으면 틀리게 된다.
         time = 99999
 
     return time
